8f6206c29d0e479c5dc96f1304d45e1e5790004b
Thanik <thanik@gmail.com>
2019-10-20 19:42:59 +0100

    Update sorter #2

--- keyaki/src/js/data/2019-10-20.js
+++ keyaki/src/js/data/2019-10-20.js
@@ -6,7 +6,8 @@ dataSet[dataSetVersion].characterData = [
         name: 'Ishimori Nijika',
         img: 'y6ZssOE.jpg',
         opts: {
-            subkeyaki: ['keyaki']
+            subkeyaki: ['keyaki'],
+            status: ['grad']
         }
     },
     {
@@ -20,49 +21,56 @@ dataSet[dataSetVersion].characterData = [
         name: 'Uemura Rina',
         img: 't3MpRxn.jpg',
         opts: {
-            subkeyaki: ['keyaki']
+            subkeyaki: ['keyaki'],
+            status: ['grad']
         }
     },
     {
         name: 'Ozeki Rika',
         img: 'xsYYGLU.jpg',
         opts: {
-            subkeyaki: ['keyaki']
+            subkeyaki: ['keyaki'],
+            status: ['grad']
         }
     },
     {
         name: 'Oda Nana',
         img: 'xP2JXqU.jpg',
         opts: {
-            subkeyaki: ['keyaki']
+            subkeyaki: ['keyaki'],
+            status: ['grad']
         }
     },
     {
         name: 'Koike Minami',
         img: 'rC4rwJg.jpg',
         opts: {
-            subkeyaki: ['keyaki']
+            subkeyaki: ['keyaki'],
+            status: ['grad']
         }
     },
     {
         name: 'Kobayashi Yui',
         img: 'TzD40M4.jpg',
         opts: {
-            subkeyaki: ['keyaki']
+            subkeyaki: ['keyaki'],
+            status: ['grad']
         }
     },
     {
         name: 'Saito Fuyuka',
         img: 'vi1m1PT.jpg',
         opts: {
-            subkeyaki: ['keyaki']
+            subkeyaki: ['keyaki'],
+            status: ['grad']
         }
     },
     {
         name: 'Sato Shiori',
         img: 'drV2j5k.jpg',
         opts: {
-            subkeyaki: ['keyaki']
+            subkeyaki: ['keyaki'],
+            status: ['grad']
         }
     },
     {
@@ -76,21 +84,24 @@ dataSet[dataSetVersion].characterData = [
         name: 'Sugai Yuuka',
         img: 'mLGPj3V.jpg',
         opts: {
-            subkeyaki: ['keyaki']
+            subkeyaki: ['keyaki'],
+            status: ['grad']
         }
     },
     {
         name: 'Suzumoto Miyu',
         img: 'dlvoyPE.jpg',
         opts: {
-            subkeyaki: ['keyaki']
+            subkeyaki: ['keyaki'],
+            status: ['grad']
         }
     },
     {
         name: 'Nagasawa Nanako',
         img: 'YIPc4PT.jpg',
         opts: {
-            subkeyaki: ['keyaki']
+            subkeyaki: ['keyaki'],
+            status: ['grad']
         }
     },
     {
@@ -104,28 +115,32 @@ dataSet[dataSetVersion].characterData = [
         name: 'Habu Mizuho',
         img: 'R4MHZwc.jpg',
         opts: {
-            subkeyaki: ['keyaki']
+            subkeyaki: ['keyaki'],
+            status: ['grad']
         }
     },
     {
         name: 'Harada Aoi',
         img: 'CG15aSQ.jpg',
         opts: {
-            subkeyaki: ['keyaki']
+            subkeyaki: ['keyaki'],
+            status: ['grad']
         }
     },
     {
         name: 'Hirate Yurina',
         img: '3Hjv31j.jpg',
         opts: {
-            subkeyaki: ['keyaki']
+            subkeyaki: ['keyaki'],
+            status: ['grad']
         }
     },
     {
         name: 'Moriya Akane',
         img: 'VM7TQY5.jpg',
         opts: {
-            subkeyaki: ['keyaki']
+            subkeyaki: ['keyaki'],
+            status: ['grad']
         }
     },
     {
@@ -139,91 +154,104 @@ dataSet[dataSetVersion].characterData = [
         name: 'Watanabe Rika',
         img: 'iFBbPeK.jpg',
         opts: {
-            subkeyaki: ['keyaki']
+            subkeyaki: ['keyaki'],
+            status: ['grad']
         }
     },
     {
         name: 'Watanabe Risa',
         img: 'U34b7Vg.jpg',
         opts: {
-            subkeyaki: ['keyaki']
+            subkeyaki: ['keyaki'],
+            status: ['grad']
         }
     },
     {
         name: 'Inoue Rina',
         img: '5nMjVUA.jpg',
         opts: {
-            subkeyaki: ['keyaki']
+            subkeyaki: ['keyaki'],
+            status: ['grad']
         }
     },
     {
         name: 'Seki Yumiko',
         img: '1rcGom7.jpg',
         opts: {
-            subkeyaki: ['keyaki']
+            subkeyaki: ['keyaki'],
+            status: ['grad']
         }
     },
     {
         name: 'Takemoto Yui',
         img: 'VeYawfV.jpg',
         opts: {
-            subkeyaki: ['keyaki']
+            subkeyaki: ['keyaki'],
+            status: ['grad']
         }
     },
     {
         name: 'Tamura Hono',
         img: 'NWloFvL.jpg',
         opts: {
-            subkeyaki: ['keyaki']
+            subkeyaki: ['keyaki'],
+            status: ['grad']
         }
     },
     {
         name: 'Fujiyoshi Karin',
         img: 'vdcQFbV.jpg',
         opts: {
-            subkeyaki: ['keyaki']
+            subkeyaki: ['keyaki'],
+            status: ['grad']
         }
     },
     {
         name: 'Matsuda Rina',
         img: 'j4e1Fjd.jpg',
         opts: {
-            subkeyaki: ['keyaki']
+            subkeyaki: ['keyaki'],
+            status: ['grad']
         }
     },
     {
         name: 'Matsudaira Riko',
         img: 'vhxki9X.jpg',
         opts: {
-            subkeyaki: ['keyaki']
+            subkeyaki: ['keyaki'],
+            status: ['grad']
         }
     },
     {
         name: 'Morita Hikaru',
         img: 'aklvjXw.jpg',
         opts: {
-            subkeyaki: ['keyaki']
+            subkeyaki: ['keyaki'],
+            status: ['grad']
         }
     },
     {
         name: 'Yamasaki Ten',
         img: 'j7IxU19.jpg',
         opts: {
-            subkeyaki: ['keyaki']
+            subkeyaki: ['keyaki'],
+            status: ['grad']
         }
     },
     {
         name: 'Iguchi Mao',
         img: 'l0SAazZ.jpg',
         opts: {
-            subkeyaki: ['hinata']
+            subkeyaki: ['hinata'],
+            status: ['grad']
         }
     },
     {
         name: 'Ushio Sarina',
         img: 'RFsItKy.jpg',
         opts: {
-            subkeyaki: ['hinata']
+            subkeyaki: ['hinata'],
+            status: ['grad']
         }
     },
     {
@@ -237,126 +265,144 @@ dataSet[dataSetVersion].characterData = [
         name: 'Kageyama Yuuka',
         img: 'EVeArCe.jpg',
         opts: {
-            subkeyaki: ['hinata']
+            subkeyaki: ['hinata'],
+            status: ['grad']
         }
     },
     {
         name: 'Kato Shiho',
         img: 'jqwcdjz.jpg',
         opts: {
-            subkeyaki: ['hinata']
+            subkeyaki: ['hinata'],
+            status: ['grad']
         }
     },
     {
         name: 'Saito Kyoko',
         img: 'j5SpbHr.jpg',
         opts: {
-            subkeyaki: ['hinata']
+            subkeyaki: ['hinata'],
+            status: ['grad']
         }
     },
     {
         name: 'Sasaki Kumi',
         img: 'uKpWEn5.jpg',
         opts: {
-            subkeyaki: ['hinata']
+            subkeyaki: ['hinata'],
+            status: ['grad']
         }
     },
     {
         name: 'Sasaki Mirei',
         img: 'djm0eyk.jpg',
         opts: {
-            subkeyaki: ['hinata']
+            subkeyaki: ['hinata'],
+            status: ['grad']
         }
     },
     {
         name: 'Takase Mana',
         img: '0qoOWGF.jpg',
         opts: {
-            subkeyaki: ['hinata']
+            subkeyaki: ['hinata'],
+            status: ['grad']
         }
     },
     {
         name: 'Takamoto Ayaka',
         img: 'rTQDCI0.jpg',
         opts: {
-            subkeyaki: ['hinata']
+            subkeyaki: ['hinata'],
+            status: ['grad']
         }
     },
     {
         name: 'Higashimura Mei',
         img: 'Vu341r5.jpg',
         opts: {
-            subkeyaki: ['hinata']
+            subkeyaki: ['hinata'],
+            status: ['grad']
         }
     },
     {
         name: 'Kanemura Miku',
         img: 'kickRAN.jpg',
         opts: {
-            subkeyaki: ['hinata']
+            subkeyaki: ['hinata'],
+            status: ['grad']
         }
     },
     {
         name: 'Kawata Hina',
         img: '0hiSJar.jpg',
         opts: {
-            subkeyaki: ['hinata']
+            subkeyaki: ['hinata'],
+            status: ['grad']
         }
     },
     {
         name: 'Kosaka Nao',
         img: 'YExxBZJ.jpg',
         opts: {
-            subkeyaki: ['hinata']
+            subkeyaki: ['hinata'],
+            status: ['grad']
         }
     },
     {
         name: 'Tomita Suzuka',
         img: 'RKzjSGc.jpg',
         opts: {
-            subkeyaki: ['hinata']
+            subkeyaki: ['hinata'],
+            status: ['grad']
         }
     },
     {
         name: 'Nibu Akari',
         img: 'uewUmao.jpg',
         opts: {
-            subkeyaki: ['hinata']
+            subkeyaki: ['hinata'],
+            status: ['grad']
         }
     },
     {
         name: 'Hamagishi Hiyori',
         img: 'q5M8FgQ.jpg',
         opts: {
-            subkeyaki: ['hinata']
+            subkeyaki: ['hinata'],
+            status: ['grad']
         }
     },
     {
         name: 'Matsuda Konoka',
         img: '3Na2LjX.jpg',
         opts: {
-            subkeyaki: ['hinata']
+            subkeyaki: ['hinata'],
+            status: ['grad']
         }
     },
     {
         name: 'Miyata Manamo',
         img: 'glEKYpb.jpg',
         opts: {
-            subkeyaki: ['hinata']
+            subkeyaki: ['hinata'],
+            status: ['grad']
         }
     },
     {
         name: 'Watanabe Miho',
         img: 'iz9lNdk.jpg',
         opts: {
-            subkeyaki: ['hinata']
+            subkeyaki: ['hinata'],
+            status: ['grad']
         }
     },
     {
         name: 'Kamimura Hinano',
         img: '93o5qOG.jpg',
         opts: {
-            subkeyaki: ['hinata']
+            subkeyaki: ['hinata'],
+            status: ['grad']
         }
     }
 ];
@@ -369,7 +415,16 @@ dataSet[dataSetVersion].options = [
         checked: false,
         sub: [
             { name: 'Keyakizaka46', key: 'keyaki'},
-            { name: 'Hinatazaka46', key: 'hinata'}
+            { name: 'Hinatazaka46', key: 'hinata'},
+        ]
+    },
+    {
+        name: 'Filter by Status',
+        key: 'status',
+        tooltip: 'Check this to restrict to certain status',
+        checked: true,
+        sub: [
+            { name: 'Exclude Graduated', key: 'grad'},
         ]
     }
 ];
