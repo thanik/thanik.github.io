7b144b717d546a4ab7b7910eab1d1a4cd0ff08ee
Thanik <thanik@gmail.com>
2020-02-08 18:27:47 +0000

    update keyaki sorter

---
+++ keyaki/src/js/data/2020-02-09.js
@@ -0,0 +1,438 @@
+dataSetVersion = "2020-02-09";
+dataSet[dataSetVersion] = {};
+
+dataSet[dataSetVersion].characterData = [
+    {
+        name: 'Ishimori Nijika',
+        img: 'y6ZssOE.jpg',
+        opts: {
+            subkeyaki: ['keyaki'],
+            status: ['ungrad']
+        }
+    },
+    {
+        name: 'Imaizumi Yui',
+        img: '7yaXuEW.jpg',
+        opts: {
+            subkeyaki: ['keyaki'],
+	    status: ['grad']
+        }
+    },
+    {
+        name: 'Uemura Rina',
+        img: 't3MpRxn.jpg',
+        opts: {
+            subkeyaki: ['keyaki'],
+            status: ['ungrad']
+        }
+    },
+    {
+        name: 'Ozeki Rika',
+        img: 'xsYYGLU.jpg',
+        opts: {
+            subkeyaki: ['keyaki'],
+            status: ['ungrad']
+        }
+    },
+    {
+        name: 'Oda Nana',
+        img: 'xP2JXqU.jpg',
+        opts: {
+            subkeyaki: ['keyaki'],
+	    status: ['grad']
+        }
+    },
+    {
+        name: 'Koike Minami',
+        img: 'rC4rwJg.jpg',
+        opts: {
+            subkeyaki: ['keyaki'],
+            status: ['ungrad']
+        }
+    },
+    {
+        name: 'Kobayashi Yui',
+        img: 'TzD40M4.jpg',
+        opts: {
+            subkeyaki: ['keyaki'],
+            status: ['ungrad']
+        }
+    },
+    {
+        name: 'Saito Fuyuka',
+        img: 'vi1m1PT.jpg',
+        opts: {
+            subkeyaki: ['keyaki'],
+            status: ['ungrad']
+        }
+    },
+    {
+        name: 'Sato Shiori',
+        img: 'drV2j5k.jpg',
+        opts: {
+            subkeyaki: ['keyaki'],
+            status: ['ungrad']
+        }
+    },
+    {
+        name: 'Shida Manaka',
+        img: 'IQQinsJ.jpg',
+        opts: {
+            subkeyaki: ['keyaki'],
+	    status: ['grad']
+        }
+    },
+    {
+        name: 'Sugai Yuuka',
+        img: 'mLGPj3V.jpg',
+        opts: {
+            subkeyaki: ['keyaki'],
+            status: ['ungrad']
+        }
+    },
+    {
+        name: 'Suzumoto Miyu',
+        img: 'dlvoyPE.jpg',
+        opts: {
+            subkeyaki: ['keyaki'],
+	    status: ['grad']
+        }
+    },
+    {
+        name: 'Nagasawa Nanako',
+        img: 'YIPc4PT.jpg',
+        opts: {
+            subkeyaki: ['keyaki'],
+            status: ['ungrad']
+        }
+    },
+    {
+        name: 'Nagahama Neru',
+        img: 'GB8elzb.jpg',
+        opts: {
+            subkeyaki: ['keyaki'],
+	    status: ['grad']
+        }
+    },
+    {
+        name: 'Habu Mizuho',
+        img: 'R4MHZwc.jpg',
+        opts: {
+            subkeyaki: ['keyaki'],
+            status: ['ungrad']
+        }
+    },
+    {
+        name: 'Harada Aoi',
+        img: 'CG15aSQ.jpg',
+        opts: {
+            subkeyaki: ['keyaki'],
+            status: ['ungrad']
+        }
+    },
+    {
+        name: 'Hirate Yurina',
+        img: '3Hjv31j.jpg',
+        opts: {
+            subkeyaki: ['keyaki'],
+	    status: ['grad']
+        }
+    },
+    {
+        name: 'Moriya Akane',
+        img: 'VM7TQY5.jpg',
+        opts: {
+            subkeyaki: ['keyaki'],
+            status: ['ungrad']
+        }
+    },
+    {
+        name: 'Yonetani Nanami',
+        img: '4WhFhTS.jpg',
+        opts: {
+            subkeyaki: ['keyaki'],
+	    status: ['grad']
+        }
+    },
+    {
+        name: 'Watanabe Rika',
+        img: 'iFBbPeK.jpg',
+        opts: {
+            subkeyaki: ['keyaki'],
+            status: ['ungrad']
+        }
+    },
+    {
+        name: 'Watanabe Risa',
+        img: 'U34b7Vg.jpg',
+        opts: {
+            subkeyaki: ['keyaki'],
+            status: ['ungrad']
+        }
+    },
+    {
+        name: 'Inoue Rina',
+        img: 'MGMMYSu.jpg',
+        opts: {
+            subkeyaki: ['keyaki'],
+            status: ['ungrad']
+        }
+    },
+    {
+        name: 'Seki Yumiko',
+        img: 'vkyc51o.jpg',
+        opts: {
+            subkeyaki: ['keyaki'],
+            status: ['ungrad']
+        }
+    },
+    {
+        name: 'Takemoto Yui',
+        img: 'OeahZQw.jpg',
+        opts: {
+            subkeyaki: ['keyaki'],
+            status: ['ungrad']
+        }
+    },
+    {
+        name: 'Tamura Hono',
+        img: 'OnaxqIF.jpg',
+        opts: {
+            subkeyaki: ['keyaki'],
+            status: ['ungrad']
+        }
+    },
+    {
+        name: 'Fujiyoshi Karin',
+        img: 'A3praFg.jpg',
+        opts: {
+            subkeyaki: ['keyaki'],
+            status: ['ungrad']
+        }
+    },
+    {
+        name: 'Matsuda Rina',
+        img: 'TTGNz0h.jpg',
+        opts: {
+            subkeyaki: ['keyaki'],
+            status: ['ungrad']
+        }
+    },
+    {
+        name: 'Matsudaira Riko',
+        img: 'qXTpcsf.jpg',
+        opts: {
+            subkeyaki: ['keyaki'],
+            status: ['ungrad']
+        }
+    },
+    {
+        name: 'Morita Hikaru',
+        img: 'VCjBn0R.jpg',
+        opts: {
+            subkeyaki: ['keyaki'],
+            status: ['ungrad']
+        }
+    },
+    {
+        name: 'Yamasaki Ten',
+        img: 'sJIbpT7.jpg',
+        opts: {
+            subkeyaki: ['keyaki'],
+            status: ['ungrad']
+        }
+    },
+    {
+        name: 'Iguchi Mao',
+        img: 'l0SAazZ.jpg',
+        opts: {
+            subkeyaki: ['hinata'],
+            status: ['ungrad']
+        }
+    },
+    {
+        name: 'Ushio Sarina',
+        img: 'h8yKfZJ.jpg',
+        opts: {
+            subkeyaki: ['hinata'],
+            status: ['ungrad']
+        }
+    },
+    {
+        name: 'Kakizaki Memi',
+        img: 'oquiaEE.jpg',
+        opts: {
+            subkeyaki: ['hinata'],
+	    status: ['grad']
+        }
+    },
+    {
+        name: 'Kageyama Yuuka',
+        img: 'EVeArCe.jpg',
+        opts: {
+            subkeyaki: ['hinata'],
+            status: ['ungrad']
+        }
+    },
+    {
+        name: 'Kato Shiho',
+        img: 'fr5BsB2.jpg',
+        opts: {
+            subkeyaki: ['hinata'],
+            status: ['ungrad']
+        }
+    },
+    {
+        name: 'Saito Kyoko',
+        img: 'vbesDDX.jpg',
+        opts: {
+            subkeyaki: ['hinata'],
+            status: ['ungrad']
+        }
+    },
+    {
+        name: 'Sasaki Kumi',
+        img: 'MkSU3Vf.jpg',
+        opts: {
+            subkeyaki: ['hinata'],
+            status: ['ungrad']
+        }
+    },
+    {
+        name: 'Sasaki Mirei',
+        img: 'QHzJabx.jpg',
+        opts: {
+            subkeyaki: ['hinata'],
+            status: ['ungrad']
+        }
+    },
+    {
+        name: 'Takase Mana',
+        img: '5SvdTHj.jpg',
+        opts: {
+            subkeyaki: ['hinata'],
+            status: ['ungrad']
+        }
+    },
+    {
+        name: 'Takamoto Ayaka',
+        img: '0oI302h.jpg',
+        opts: {
+            subkeyaki: ['hinata'],
+            status: ['ungrad']
+        }
+    },
+    {
+        name: 'Higashimura Mei',
+        img: 'MvRDAut.jpg',
+        opts: {
+            subkeyaki: ['hinata'],
+            status: ['ungrad']
+        }
+    },
+    {
+        name: 'Kanemura Miku',
+        img: 'V6CttjU.jpg',
+        opts: {
+            subkeyaki: ['hinata'],
+            status: ['ungrad']
+        }
+    },
+    {
+        name: 'Kawata Hina',
+        img: 'sJJSmke.jpg',
+        opts: {
+            subkeyaki: ['hinata'],
+            status: ['ungrad']
+        }
+    },
+    {
+        name: 'Kosaka Nao',
+        img: '0DDxo8h.jpg',
+        opts: {
+            subkeyaki: ['hinata'],
+            status: ['ungrad']
+        }
+    },
+    {
+        name: 'Tomita Suzuka',
+        img: 'aHy56GQ.jpg',
+        opts: {
+            subkeyaki: ['hinata'],
+            status: ['ungrad']
+        }
+    },
+    {
+        name: 'Nibu Akari',
+        img: 'mm7hzey.jpg',
+        opts: {
+            subkeyaki: ['hinata'],
+            status: ['ungrad']
+        }
+    },
+    {
+        name: 'Hamagishi Hiyori',
+        img: 'q5M8FgQ.jpg',
+        opts: {
+            subkeyaki: ['hinata'],
+            status: ['ungrad']
+        }
+    },
+    {
+        name: 'Matsuda Konoka',
+        img: 'FCRIhxD.jpg',
+        opts: {
+            subkeyaki: ['hinata'],
+            status: ['ungrad']
+        }
+    },
+    {
+        name: 'Miyata Manamo',
+        img: 'xhG0jRH.jpg',
+        opts: {
+            subkeyaki: ['hinata'],
+            status: ['ungrad']
+        }
+    },
+    {
+        name: 'Watanabe Miho',
+        img: 'iXl1PK3.jpg',
+        opts: {
+            subkeyaki: ['hinata'],
+            status: ['ungrad']
+        }
+    },
+    {
+        name: 'Kamimura Hinano',
+        img: 'C2Lz9Im.jpg',
+        opts: {
+            subkeyaki: ['hinata'],
+            status: ['ungrad']
+        }
+    }
+];
+
+dataSet[dataSetVersion].options = [
+    {
+        name: 'Filter by Group',
+        key: 'subkeyaki',
+        tooltip: 'Check this to restrict to certain group',
+        checked: false,
+        sub: [
+            { name: 'Keyakizaka46', key: 'keyaki'},
+            { name: 'Hinatazaka46', key: 'hinata'},
+        ]
+    },
+    {
+        name: 'Filter by Graduation Status',
+        key: 'status',
+        tooltip: 'Check this to include/exclude members by graduation status',
+        checked: false,
+        sub: [
+           
+            { name: 'Graduated Members', key: 'grad'},
+            { name: 'Current Members', key: 'ungrad'}
+        ]
+    }
+];
+
